4ffa3a45848f20fa3582767d623b9460a117e64b
{ Alessandro :  Alê } <48811968+alessandrop76@users.noreply.github.com>
2022-11-21 18:52:35 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-//const connection = new Sequelize('guiapress','root','relogio',{
+
 const connection = new Sequelize('ma','ma','rel',{
     //host: 'localhost',
     host: 'mysql743.umbler.com',
